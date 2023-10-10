d30a4931f85cbb9bbf6192af229d0b2f0d695ab2
Angel Angeles <daniel_1234@hotmail.es>
2023-10-10 12:20:11 -0400

    creating nav desing with chatgpt help

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import OrderPizza from "./screens/OrderPizza";
 
 const App = () => {
+  const [activeOption, setActiveOption] = useState('home');
+
   return (
     <>
       <BrowserRouter>
-        <h1>Lambda Eats</h1>
+        <div className="navigator">
+          <div className="logo">
+            <h4>BloomTech Eats</h4>
+          </div>
+          <div className="options">
+            <nav>
+              <Link className="nav-links" to="/">
+                <button
+                  className={activeOption === 'home' ? 'button-active' : 'button'}
+                  onClick={() => setActiveOption('home')}
+                >
+                  Home
+                </button>
+              </Link>
+              <Link className="nav-links" to="pizza">
+                <button
+                  className={activeOption === 'pizza' ? 'button-active' : 'button'}
+                  onClick={() => setActiveOption('pizza')}
+                >
+                  Create Order
+                </button>
+              </Link>
+            </nav>
+          </div>
+        </div>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="pizza/*" element={<OrderPizza/>} />
