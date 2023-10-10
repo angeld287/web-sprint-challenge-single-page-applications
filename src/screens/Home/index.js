dfa03ff8b65da1b67ac92c6acf261034d76651ac
Angel Angeles <daniel_1234@hotmail.es>
2023-10-10 14:07:46 -0400

    adding the home banner imagen at the top

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,29 @@
 import React from "react";
+import './index.css'
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const imageUrl = "https://www.saborusa.com/do/wp-content/uploads/sites/8/2019/11/Animate-a-disfrutar-una-deliciosa-pizza-de-salchicha-Foto-destacada.png";
   return (
     <>
-        <h1>HOME</h1>
-        <button id="order-pizza">Create Order</button>
+        <div className="top-home-page">
+          <div className="container">
+            <div className="background-image-container">
+              <img
+                className="background-image"
+                src={imageUrl}
+                alt="Pizza deliciosa"
+              />
+              <div className="overlay"></div>
+            </div>
+            <div className="image-center">
+              <Link to="pizza"><button id="order-pizza" className="order-button">Create Order</button></Link>
+            </div>
+          </div>
+        </div>
+        <div className="body-home-page">
+
+        </div>
     </>
   );
 };
